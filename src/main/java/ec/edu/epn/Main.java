f38f1311221a77c271e95fcd20ffd0a8de4fa443
miguelangelmuenala <49161695+miguelangelmuenala@users.noreply.github.com>
2022-06-22 00:15:46 -0500

    Commit Con el JAR

--- src/main/java/ec/edu/epn/Main.java
+++ src/main/java/ec/edu/epn/Main.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         // TODO code application logic here
+
         GestorSistema gestor = new GestorSistema();
         gestor.inicializarArchivos();
         System.out.println("Bienvenido al Sistema de Gestión de Empleados y Productos\n"
